78c6c123ba186555645440ecff7681bd93ea2c15
tyg3rr <ljensen1203@gmail.com>
2024-01-25 16:15:38 -0500

    fixed helper_functions git deletion

--- analysis/helper_functions.py
+++ analysis/helper_functions.py
@@ -1,13 +1,38 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
+import matplotlib.pyplot as plt
 
 
-def difference(dataset, n):
-    """ this function intakes a list of values, 
-    subtracts the nth prior value,
-    and returns the list of differenced values.
+def cyclical_encoding(data: pd.Series, cycle_length: int) -> pd.DataFrame:
     """
-    return pd.Series([dataset[i] - dataset[i - n] for i in range(1, len(dataset))])
+    Encode a cyclical feature with two new features sine and cosine.
+    The minimum value of the feature is assumed to be 0. The maximum value
+    of the feature is passed as an argument.
+    
+    Parameters
+    ----------
+    data : pd.Series
+        Series with the feature to encode.
+    cycle_length : int
+        The length of the cycle. For example, 12 for months, 24 for hours, etc.
+        This value is used to calculate the angle of the sin and cos.
+    Returns
+    -------
+    result : pd.DataFrame
+        Dataframe with the two new features sin and cos.
+    """
+
+    sin = np.sin(2 * np.pi * data/cycle_length)
+    cos = np.cos(2 * np.pi * data/cycle_length)
+    result =  pd.DataFrame({
+                f"{data.name}_sin": sin,
+                f"{data.name}_cos": cos
+            })
+
+    return result
+
 
 def ADF(time_series, max_lags):
     """
@@ -39,3 +64,30 @@ def results_summary_to_dataframe(results):
     #Reordering...
     results_df = results_df[["coeff","pvals","conf_lower","conf_higher"]]
     return results_df
+
+
+def plot_ccf_manual(target, exog, nlags=10):
+    """PLot CCF using manual calculations"""
+    lags = []
+    ccfs = []
+    for i in np.arange(0,nlags+1):
+        lags.append(i)
+        ccfs.append(crosscorr(target, exog, lag=i))
+
+    _ = plt.stem(lags, ccfs, use_line_collection=True)
+    _ = plt.title(f"Cross Correlation (Manual): {target.name} & {exog.name}")
+    plt.show()
+    plt.close()
+
+def crosscorr(x: pd.Series, y: pd.Series, lag: int=0) -> float:
+    """ Lag-N cross correlation. 
+    Shifted data (y) filled with NaNs 
+    Parameters
+    ----------
+    lag : int, default 0
+    x, y : pandas.Series objects of equal length
+    Returns
+    ----------
+    crosscorr : float
+    """
+    return x.corr(y.shift(lag))
